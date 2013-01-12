cd445264d85ec2b6387bd4bba60b5942cc6e3b21
Evan Bailey <cynicaloptimist@gmail.com>
2013-01-11 17:50:31 -0700

    Update functions.php
    Added a hook to get the search param to indicate we want a google search.

--- functions.php
+++ functions.php
@@ -107,27 +107,16 @@ function register_my_menus() {
 }
 add_action( 'init', 'register_my_menus' );
 
+function add_search_params() { 
+    global $wp; 
+    $wp->add_query_var('search_method'); 
+}
+add_action('init','add_search_params');
+
 //Initialize the update checker.
 require 'theme-updates/theme-update-checker.php';
 $example_update_checker = new ThemeUpdateChecker(
     'nmsu_testing',
     'http://saem.nmsu.edu/theme_updates/updatefile.json'
 );
-
-
-//Theme options panel
-
-function setup_theme_admin_menus() {
-    add_submenu_page('themes.php',
-        'Front Page Elements', 'Template Options', 'manage_options',
-        'front-page-elements', 'theme_front_page_settings');
-}
-// This tells WordPress to call the function named "setup_theme_admin_menus"
-// when it's time to create the menu pages.
-add_action("admin_menu", "setup_theme_admin_menus");
-
-function theme_front_page_settings() {
-    echo "Hello, world!";
-}
-
 ?>
